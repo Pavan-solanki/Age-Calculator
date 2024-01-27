f22d4c14286f84fb8d0cb0d0a276db99f703baa5
Pavan Solanki <138639956+Pavan-solanki@users.noreply.github.com>
2024-01-28 01:11:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+let input=document.querySelector("form input");
+input.max = new Date().toISOString().split('T')[0];
+let result=document.querySelector(".result");
+const button=document.querySelector("button");
+function calulateAge(e){
+    e.preventDefault()
+    let dob = new Date(input.value);
+
+    let d1=dob.getDate();
+    let m1=dob.getMonth()+1;
+    let y1=dob.getFullYear();
+
+    let today=new Date();
+    let d2=today.getDate();
+    let m2=today.getMonth()+1;
+    let y2=today.getFullYear();
+
+    let d3,m3,y3;
+    y3=y2-y1;
+    if(m2>=m1){
+        m3=m2-m1;
+    }else{
+        y3--;
+        m3=12+m2-m1;
+    }
+    if(d2>=d1){
+        d3=d2-d1;
+    }else{
+        m3--;
+        d3=getDaysINMonth(y1,m1)+d2-d1;
+    }
+    if(m3<0){
+        m3=11;
+        y3--;
+    }
+    
+    result.innerHTML=`You are <span>${y3}</span> years, <span>${m3}</span> months, and <span>${d3}</span>  days old`;
+}
+function getDaysINMonth(year,month){
+    return new Date(year,month,0).getDate();
+}
+button.addEventListener("click",calulateAge);
